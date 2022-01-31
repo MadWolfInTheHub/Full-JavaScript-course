e10c6cccb621b41fa407a06754396e0a8d074a8b
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-01-31 20:07:45 +0800

    test

---
+++ FullJavaCourse/lesson21/task2/index.js
@@ -0,0 +1,13 @@
+const getTitleElement = () => {
+    const TitleElement = document.querySelector('.title'); 
+    return TitleElement;
+}
+
+getTitleElement();
+
+const getInputElement = () => {
+    const inputElem = document.hasAttribute('type', 'text');
+    inputElem;
+}
+
+getINputElement();
