43213a718aa3621fc6079dd2af17479f454e17ef
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-01-31 21:05:46 +0800

    test

---
+++ FullJavaCourse/lesson21/task4/index.js
@@ -0,0 +1,20 @@
+export const getTitle = () => {
+    return document.querySelector('.title').textContent;
+}
+
+export const getDescription = () => {
+    return document.querySelector('.about').innerText;
+}
+
+export const getPlans = () => {
+    return document.querySelector('.plans').innerHTML;
+}
+
+export const getGoal = () => { 
+    return document.querySelector('.goal').outerHTML;
+}
+
+console.log(getTitle());
+console.log(getDescription());
+console.log(getPlans());
+console.log(getGoal());
