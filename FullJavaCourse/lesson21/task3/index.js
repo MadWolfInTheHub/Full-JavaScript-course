d5a1abf3fd27e46c7757784d17137675c7a7e719
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-01-31 20:53:26 +0800

    test

--- FullJavaCourse/lesson21/task3/index.js
+++ FullJavaCourse/lesson21/task3/index.js
@@ -1,14 +1,14 @@
 export const getItemsList = () => {
-    const elemetsList = document.querySelectorAll('.technology');
-    console.dir(elemetsList);
-    return elemetsList;
-}
+  const elemetsList = document.querySelectorAll(".technology");
+  console.dir(elemetsList);
+  return elemetsList;
+};
 
 export const getItemsArray = () => {
-    const elementsArray = Array.from(document.querySelectorAll('.tool')).map;
-    console.dir(elementsArray);
-    return elementsArray;
-}
+  const elementsArray = Array.from(document.querySelectorAll(".tool")).map;
+  console.dir(elementsArray);
+  return elementsArray;
+};
 
-getItemList();
+getItemsList();
 getItemsArray();
