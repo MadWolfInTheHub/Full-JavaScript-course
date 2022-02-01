ee4ff85d6e984dca9948940b49d35a8f66ee1dc0
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-01 10:30:42 +0800

    atributes

---
+++ FullJavaCourse/lesson21/task10/index.js
@@ -0,0 +1,10 @@
+export const finishform = () => {
+  const formInput = document.querySelector(".login-form");
+  const loginInput = document.createElement("input");
+  loginInput.setAttribute("type", "text");
+  loginInput.setAttribute("name", "login");
+  formInput.prepend(loginInput);
+
+  const passwordInput = document.querySelector("input[name=password]");
+  passwordInput.setAttribute("type", "password");
+};
