dd552c1d980f629656731f55948233125eba6135
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-01 18:23:48 +0800

    search field

---
+++ FullJavaCourse/lesson22/task4/index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.search__btn');
+const input = document.querySelector('.search__input');
+
+
+const search = () => {
+    console.log(input.value);
+}
+
+button.addEventListener('click', search)
