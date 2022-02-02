90558f5006b4a0eac4a3129e11e1f46063bd6ead
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-02 12:09:29 +0800

    test

--- FullJavaCourse/lesson22/task6/index.js
+++ FullJavaCourse/lesson22/task6/index.js
@@ -1,8 +1,7 @@
 const buttons = document.querySelectorAll(".btn");
 
-
 const handleClick = (event) => {
-    console.log(event.target.textContent);
+  console.log(event.target.textContent);
 };
 
 const buttonsArr = Array.from(buttons).forEach((el) => {
