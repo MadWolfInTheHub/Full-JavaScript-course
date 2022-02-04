827c34e222e0e7e3a679497ca7889e7f1ab767ca
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-04 11:12:22 +0800

    test

--- FullJavaCourse/lesson24/task1/index.js
+++ FullJavaCourse/lesson24/task1/index.js
@@ -1,8 +1,8 @@
 export const dayOfWeek = (date, days) => {
-  const weekDays = ["Mo", "Tu", "We", "Th", "Fr", "St", 'Su'];
+  const weekDays = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
   const day = new Date(date).getDate();
   const dateInFuture = new Date(day).setDate(day + days);
   return weekDays[new Date(dateInFuture).getDay()];
 };
-const result = dayOfWeek(new Date(2022, 3, 4), 13);
-console.log(result);
+/* const result = dayOfWeek(new Date(2022, 3, 4), 13);
+console.log(result); */
