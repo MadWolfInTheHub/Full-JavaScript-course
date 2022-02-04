f8f37de4556633d478879f3192d623c7f8ffaef3
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-04 17:18:41 +0800

    test

--- FullJavaCourse/lesson24/task2/index.js
+++ FullJavaCourse/lesson24/task2/index.js
@@ -3,7 +3,7 @@ const formater = new Intl.DateTimeFormat("en-GB", {
   timeZone: "UTC",
   hour: "2-digit",
   minute: "2-digit",
-  hour12: "false",
+  hour12: false,
 });
 
 export const getGreenwichTime = (date) => formater.format(date);
--- FullJavaCourse/lesson24/task3/index.js
+++ FullJavaCourse/lesson24/task3/index.js
@@ -1,15 +1,13 @@
 /* export */ const getDiff = (startDate, endDate) => {
   const first = startDate.getTime();
   const second = endDate.getTime();
-  console.log(new Date(first));
-  console.log(new Date(second));
-  const result = (second - first) / (1000 * 60 * 60 * 24);
-  first < second ? (days = Math.floor(result)) : (days = Math.ceil(result));
+  const result = second - first;
+  const days = Math.floor((result)/(1000*60*60*24));
   const hours = new Date(result).getHours();
   const minutes = new Date(result).getMinutes();
   const seconds = new Date(result).getSeconds();
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-};
+}
 
-console.log(getDiff(new Date(), new Date(2022, 1, 4)));
-console.log(new Date());
+console.log(getDiff(new Date(2022, 1, 2), new Date()));
+console.log(new Date)
