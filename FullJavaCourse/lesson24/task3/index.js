d510c0c5ee647da98941010b09ceb68bceb0d8f9
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-04 12:59:20 +0800

    date difference

---
+++ FullJavaCourse/lesson24/task3/index.js
@@ -0,0 +1,12 @@
+export const getDiff = (startDate, endDate) => {
+  const first = startDate.getTime();
+  const second = endDate.getTime();
+  const result = second - first;
+  const days = Math.floor((result)/(1000*60*60*24));
+  const hours = new Date(result).getHours();
+  const minutes = new Date(result).getMinutes();
+  const seconds = new Date(result).getSeconds();
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+/* console.log(getDiff(new Date(), new Date(2022, 1, 6))); */
