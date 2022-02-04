690dafcb4ceefb6140131f1370f0f1fe3c53a168
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-04 19:48:57 +0800

    getDayOfTheWeek

--- FullJavaCourse/lesson24/task1/index.js
+++ FullJavaCourse/lesson24/task1/index.js
@@ -1,5 +1,5 @@
 export const dayOfWeek = (date, days) => {
-  const weekDays = ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"];
+  const weekDays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "St"];
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
   return weekDays[new Date(dateInFuture).getDay()];
