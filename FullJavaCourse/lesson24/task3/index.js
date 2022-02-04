ad31538109370d0d967b361a13287caef4a62f1d
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-04 13:05:03 +0800

    dates

--- FullJavaCourse/lesson24/task3/index.js
+++ FullJavaCourse/lesson24/task3/index.js
@@ -3,10 +3,10 @@ export const getDiff = (startDate, endDate) => {
   const second = endDate.getTime();
   const result = second - first;
   const days = Math.floor((result)/(1000*60*60*24));
-  const hours = new Date(result).getHours();
+  const hours = new Date(result).getHours() - 6;
   const minutes = new Date(result).getMinutes();
   const seconds = new Date(result).getSeconds();
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
 
-/* console.log(getDiff(new Date(), new Date(2022, 1, 6))); */
+console.log(getDiff(new Date(), new Date(2022, 1, 6)));
