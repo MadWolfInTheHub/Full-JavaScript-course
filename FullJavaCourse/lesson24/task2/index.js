208e7128d52f4c78176ff2c1eb9c4cfeb52aeb99
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-04 12:13:19 +0800

    get greenweech time

--- FullJavaCourse/lesson24/task2/index.js
+++ FullJavaCourse/lesson24/task2/index.js
@@ -1,12 +1,10 @@
-export const getGreenwichTime = (date) => {
-  const formater = new Intl.DateTimeFormat("ru", {
-    timeZone: "UTC",
-    hour: "2-digit",
-    minute: "2-digit",
-    hours12: "false",
-  });
-   
-  return formater.format(date);
-};
+export const getGreenwichTime = (date) => formater.format(date);
+
+const formater = new Intl.DateTimeFormat("ru", {
+  timeZone: "UTC",
+  hour: "2-digit",
+  minute: "2-digit",
+  hours12: "false",
+});
 
 console.log(getGreenwichTime(new Date()));
