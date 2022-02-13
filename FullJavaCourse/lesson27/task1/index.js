37899e82a5fe7ee8376a9251e0b95427fe8100c9
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-13 16:49:19 +0800

    local memory test

--- FullJavaCourse/lesson27/task1/index.js
+++ FullJavaCourse/lesson27/task1/index.js
@@ -2,7 +2,7 @@ export const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
     return {
       ...acc,
-      [key]: value,
+      [key]: JSON.parse(value),
     };
   }, {});
 };
