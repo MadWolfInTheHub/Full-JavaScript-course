5d6c63ded6149e3b3c5319a6351fa5bd6e0dfc6a
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-16 11:30:13 +0800

    test

---
+++ FullJavaCourse/lesson27/task3/scripts/index.js
@@ -0,0 +1,51 @@
+import { getItem, setItem } from "./storage.js";
+import { renderTasks } from "./renderer.js";
+
+const listElem = document.querySelector(".list");
+renderTasks();
+
+/* we need to check changes in each checkbox and according to this changes change the status of listed task */
+const renderList = () => {
+  listElem.textContent = "";
+  renderTasks();
+};
+
+const newTask = (event) => {
+  const tasks = getItem("taskList");
+  const result = tasks.find(({ id }) => String(id) === event.target.dataset.id);
+  result.done = event.target.checked;
+  setItem("taskList", tasks)
+  renderList();
+};
+
+const onBoxSelect = (event) => {
+  const isCheckBox = event.target.classList.contains("list__item-checkbox");
+
+  if (!isCheckBox) {
+    return;
+  }
+  newTask(event);
+};
+listElem.addEventListener("click", onBoxSelect);
+
+/* we need to check cnages in input if its not empty we need to add text by cliking on the button 'create' to our object */
+
+const createBtn = document.querySelector(".create-task-btn");
+const addTask = () => {
+  const input = document.querySelector(".task-input");
+  let newTask;
+  if (input.value) {
+    const tasks = getItem("taskList") || [];
+    newTask = tasks.concat({
+      text: input.value,
+      done: false,
+      createDate: new Date().toISOString(),
+      id: Math.random(),
+    });
+    input.value = "";
+    setItem("taskList", newTask);
+  }
+  renderList();
+};
+createBtn.addEventListener("click", addTask);
+
