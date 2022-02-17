cdfa84617feb369f0228f0e9bd228e85f5a25228
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-17 16:34:53 +0800

    currying

---
+++ FullJavaCourse/lesson28/task3/index.js
@@ -0,0 +1,10 @@
+const mult = (a) => (b) => a * b;
+
+export const twice = (num) => mult(num)(2);
+
+export const triple = (num) => mult(num)(3); 
+
+
+console.log(mult(5)(4));
+console.log(twice(7));
+console.log(triple(3));
