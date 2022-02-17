6f521b8eb0f29db170c7c45cbb88fa5bd3e9988b
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-17 18:25:18 +0800

    maxFibonachi num

---
+++ FullJavaCourse/lesson28/task6/index.js
@@ -0,0 +1,18 @@
+export const maxFibonachi = (num) => {
+  const sum = (a) => (b) => a + b;
+  let amountToDecrease = 0;
+  let previousNUmber = 1;
+  let currentNUmber = 1;
+  let res = 0;
+
+  while (res <= num) {
+    res = sum(previousNUmber)(currentNUmber);
+    amountToDecrease = previousNUmber;
+    previousNUmber = currentNUmber;
+    currentNUmber = res;
+    console.log(res)
+  }
+  return res - amountToDecrease;
+};
+
+console.log(maxFibonachi(8));
