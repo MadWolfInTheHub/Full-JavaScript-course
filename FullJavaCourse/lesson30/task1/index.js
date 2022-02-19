ee8a756669990236dd7203e9267fdbee7768914e
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-19 12:35:05 +0800

    test

--- FullJavaCourse/lesson30/task1/index.js
+++ FullJavaCourse/lesson30/task1/index.js
@@ -1,4 +1,4 @@
-export const addImg = (imgSrc) => {
+export const addImge = (imgSrc) => {
   const p = new Promise((resolve, rejected) => {
     const imgElem = document.createElement("img");
     imgElem.setAttribute("alt", "My Photo");
@@ -21,6 +21,6 @@ export const addImg = (imgSrc) => {
 
 const imgSrc =
   "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
-const resultPromise = addImg(imgSrc);
+const resultPromise = addImge(imgSrc);
 resultPromise.then((data) => console.log(data));
 resultPromise.catch((error) => console.log(error));
