deb841aacb5978ac77de830d00f4e6be7a8d37e0
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-19 12:52:36 +0800

    promise test

--- FullJavaCourse/lesson30/task2/index.js
+++ FullJavaCourse/lesson30/task2/index.js
@@ -20,7 +20,7 @@ export const addImageV2 = (url) => {
 };
 
 
-/* addImageV2("https://server.com/image.png")
+addImageV2("https://server.com/image.png")
   .then((data) => console.log(data)) // ==> { width: 200, height: 100 }
-  .catch((error) => console.log(error)); // ==> 'Image load failed' */
+  .catch((error) => console.log(error)); // ==> 'Image load failed'
 
