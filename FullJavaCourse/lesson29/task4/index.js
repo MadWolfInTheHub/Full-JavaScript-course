d742f7d9dd7c06ef5f709b59867f2cc5fed974d5
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-19 12:09:03 +0800

    test

---
+++ FullJavaCourse/lesson29/task4/index.js
@@ -0,0 +1,5 @@
+const daley = (delay, callack, context, ...arguments) => {
+
+}
+
+delay(3000, printText(), )
---
+++ FullJavaCourse/lesson30/task1/index.js
@@ -0,0 +1,26 @@
+const addImg = (imgSrc) => {
+  const p = new Promise((resolve, rejected) => {
+    const imgElem = document.createElement("img");
+    imgElem.setAttribute("alt", "My Photo");
+    imgElem.src = imgSrc;
+
+    const containerElem = document.querySelector(".page");
+    containerElem.append(imgElem);
+    const onImgChange = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener("load", onImgChange);
+    imgElem.addEventListener("error", () =>
+      rejected(new Error("Image load is failed..."))
+    );
+  });
+  return p;
+};
+
+const imgSrc =
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
+const resultPromise = addImg(imgSrc);
+resultPromise.then((data) => console.log(data));
+resultPromise.catch((error) => console.log(error));
