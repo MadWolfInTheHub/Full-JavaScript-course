0e16bd0d444af865762bf5a464605951558c69c0
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-02-21 12:38:05 +0800

    promise wrap

---
+++ FullJavaCourse/lesson32/task3/index.js
@@ -0,0 +1,17 @@
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+export const makePromise = (num) => {
+  const p = new Promise((resolve) => {
+    resolve(num);
+  });
+  return p;
+};
+
+/*
+ * пример использования
+ */
+makePromise(17).then((number) => {
+  console.log(number); // 17
+});
