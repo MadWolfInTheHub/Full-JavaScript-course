e4bde36f355bf47a43b5e023a010e447173c3ce1
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-10 22:45:30 +0800

    1

--- FullJavaCourse/lesson33/task2/index.js
+++ FullJavaCourse/lesson33/task2/index.js
@@ -7,3 +7,11 @@ export function getTasksList() {
 export function getTaskById(taskId) {
   return getTasksList().then((data) => data.find((el) => el.id === taskId));
 }
+
+getTasksList().then(tasksList => {
+  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
+});
+
+getTaskById('2').then(taskData => {
+  console.log(taskData); // {'id':'2', 'done':true ... }
+});
