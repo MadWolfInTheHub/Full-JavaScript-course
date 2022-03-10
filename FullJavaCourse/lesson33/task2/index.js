3d4a2e42928a7b3a3c1205a3a64ab94c25a017eb
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-10 22:33:15 +0800

    get tasks info

--- FullJavaCourse/lesson33/task2/index.js
+++ FullJavaCourse/lesson33/task2/index.js
@@ -1,21 +1,9 @@
 const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
 
 export function getTasksList() {
-  return fetch(baseUrl)
-  .then((response) => response.json())
+  return fetch(baseUrl).then((response) => response.json());
 }
 
 export function getTaskById(taskId) {
-  return fetch(baseUrl)
-  .then((response) => response.json())
-  .then(data => data.filter(el => el.id === taskId))
+  return getTasksList().then((data) => data.filter((el) => el.id === taskId));
 }
-
-// examples
-getTasksList().then((tasksList) => {
-  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
-});
-
-getTaskById("2").then((taskData) => {
-  console.log(taskData); // {'id':'2', 'done':true ... }
-});
