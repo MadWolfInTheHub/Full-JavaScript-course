56187bdab38a46890b8921a7ad83e9eec2d55da8
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-13 18:15:30 +0800

    test

--- FullJavaCourse/lesson34/task3/index.js
+++ FullJavaCourse/lesson34/task3/index.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://crudcrud.com/Dashboard/a21dd6d95bbe48a29d7428da8cf8371e";
+const baseUrl =
+  "https://crudcrud.com/api/a21dd6d95bbe48a29d7428da8cf8371e/users";
 
 const emailEL = document.querySelector("[name=email]");
 const userNameEl = document.querySelector("[name=name]");
@@ -6,14 +7,19 @@ const passwordEl = document.querySelector("[name=password]");
 const submitForm = document.querySelector(".login-form");
 const registerBtn = document.querySelector(".submit-button");
 
-function createUser(userData) {
+const createUser = (userData) => {
+  console.log(userData);
   return fetch(baseUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json=utf-8",
     },
-    body: JSON.stringify(userData),
+    body: userData,
   });
+};
+
+function getUsersList() {
+  return fetch(baseUrl).then((response) => response.json());
 }
 
 const onRegisterUser = () => {
@@ -21,21 +27,25 @@ const onRegisterUser = () => {
     email: emailEL.value,
     userName: userNameEl.value,
     password: passwordEl.value,
+    id: Math.random().toString(),
   };
-  createUser(newUser).then(() => {
-    alert(newUser);
+
+  createUser(JSON.stringify(newUser)).then(() => {
+    console.log("User created");
   });
+  alert(getUsersList());
 };
 
 const onFormsubmit = () => {
   submitForm.reportValidity() === true
     ? (registerBtn.disabled = false)
     : (registerBtn.disabled = true);
-    
-    if(!registerBtn.hasAttribute('disabled')) {
-        registerBtn.setAttribute('enabled', '')
-    }
+
+  if (!registerBtn.hasAttribute("disabled")) {
+    registerBtn.setAttribute("enabled", "");
+  }
 };
 
 submitForm.addEventListener("input", onFormsubmit);
-registerBtn.addEventListener("click", onRegisterUser);
+submitForm.addEventListener("submit", onRegisterUser);
+/* registerBtn.addEventListener("submit", onRegisterUser); */
