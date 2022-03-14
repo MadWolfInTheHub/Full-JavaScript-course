f89ceaaaafe7f2ec8d47f93d4d4ad23a32801894
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-14 16:57:30 +0800

    test

--- FullJavaCourse/lesson34/task3/index.js
+++ FullJavaCourse/lesson34/task3/index.js
@@ -50,6 +50,6 @@ const onFormsubmit = (e) => {
 };
 
 
-submitForm.addEventListener("change", onFormsubmit);
+submitForm.addEventListener("input", onFormsubmit);
 submitForm.addEventListener("submit", onRegisterUser);
 /* registerBtn.addEventListener("submit", onRegisterUser); */
