3bbb0d0bef51b7d0f250925a9f84b8d291317a20
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-15 18:32:02 +0800

    test

--- FullJavaCourse/lesson35/task3/spinner.js
+++ FullJavaCourse/lesson35/task3/spinner.js
@@ -1,7 +1,7 @@
 const spinnerElement = document.querySelector(".spinner");
 
 export const hideSpinner = () => {
-  spinnerElement.classList.remove(".spinner_hidden");
+  spinnerElement.classList.remove("spinner_hidden");
 };
 
 export const showSpinner = () => {
