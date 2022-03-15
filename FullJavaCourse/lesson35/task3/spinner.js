cca768f9ea1175e78815de433b27d5a629af81eb
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-15 18:30:36 +0800

    gitHub repos list

---
+++ FullJavaCourse/lesson35/task3/spinner.js
@@ -0,0 +1,9 @@
+const spinnerElement = document.querySelector(".spinner");
+
+export const hideSpinner = () => {
+  spinnerElement.classList.remove(".spinner_hidden");
+};
+
+export const showSpinner = () => {
+  spinnerElement.classList.add("spinner_hidden");
+};
