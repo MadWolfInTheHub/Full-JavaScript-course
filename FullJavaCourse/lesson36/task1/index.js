3b0f5b87e8a5f9a301f50148a2f28bd138eed654
Serhii <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-03-17 14:16:31 +0800

    test

--- FullJavaCourse/lesson36/task1/index.js
+++ FullJavaCourse/lesson36/task1/index.js
@@ -1,20 +1,14 @@
-export const fetchUser = async (userId) => {
+/* export */ const fetchUser = async (userId) => {
   try {
     const response = await fetch(`https://api.github.com/users/${userId}`);
     if (!response.ok) {
-      /* console.log(null); */
       return null;
     }
-
     const userData = await response.json();
-    /* console.log(userData); */
     return userData;
   } catch (err) {
-    /* console.log(err) */;
-    throw new Error("Failed to get uaser data");
+    throw new Error("Failed to get user data");
   }
 };
 
-/* fetchUser("facebook")
-.then((userData) => console.log(userData))
-.catch((err) => err.message); */
+fetchUser("facebook");
